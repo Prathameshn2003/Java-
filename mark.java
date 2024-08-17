da510433d0151b77e0db1df925e4e03ef94636b4
Prathameshn2003 <prathameshnalawade288@gmail.com>
2024-08-17 23:18:48 +0530

    Add files via upload

---
+++ mark.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class mark{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int  sum=0;
+        while(num>0)
+        {
+            int rem=num%10;
+            sum=sum+rem;
+            num=num/10;
+        }
+        System.out.println("sum of number"+sum);
+    }
+}
