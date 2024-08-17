b0437103038b10c11148bc7daf763ba799d7bccf
Prathameshn2003 <prathameshnalawade288@gmail.com>
2024-08-17 14:17:18 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class EvenOdd{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number");
+        int num=sc.nextInt();
+        int evennum=0;
+        int oddnum=0;
+        for(int i=0;i<=num;i++)
+        {
+            if(i%2==0)
+            {
+                evennum=evennum+i;
+            }else{
+                oddnum=oddnum+i;
+            }
+        }
+        System.out.println("even number sum"+evennum);
+        System.out.println("odd number sum"+oddnum);
+    }
+}
