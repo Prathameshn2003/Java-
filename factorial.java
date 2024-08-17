1641ee5080f9c8e68ad89074b4fc214cdce0807b
Prathameshn2003 <prathameshnalawade288@gmail.com>
2024-08-17 14:46:13 +0530

    Add files via upload

--- factorial.java
+++ factorial.java
@@ -3,13 +3,26 @@ public class factorial{
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter number");
-        int num=sc.nextInt();
-        int fact=1;
-        for(int i=1;i<=num;i++)
-        {
-            fact=fact*i;
+        System.out.println("Enter value of a");
+        int a=sc.nextInt();
+        System.out.println("Enter value of b");
+        int b=sc.nextInt();
+        int c;
+        System.out.println("choose operator");
+        int ch=sc.next().charAt(0); 
+        if(ch=='+')   {
+            c=a+b;
+            System.out.println("addtion is"+c);
+        } else if(ch=='-'){
+            c=a-b;
+            System.out.println("sub is"+c);
+        }else if(ch=='*'){
+            c=a*b;
+            System.out.println("mul is"+c);
+        }else if(ch=='/'){
+            c=a/b;
+            System.out.println("div is"+c);
         }
-        System.out.println("factorial of"+num+"is"+fact);
+        
     
 }}
