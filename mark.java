80648ac85a318e25671ae83a8b1d07464c017e59
Prathameshn2003 <prathameshnalawade288@gmail.com>
2024-08-17 23:25:25 +0530

    Add files via upload

---
+++ mark.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class mark{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int  evensum=0;
+        int oddsum=0;
+        while(num>0)
+        {
+            int rem=num%10;
+            if(rem%2==0)
+            {
+                evensum=evensum+rem;
+            }else{
+                oddsum=oddsum+rem;
+            }
+            
+            num=num/10;
+        }
+        System.out.println("even number sum"+evensum);
+        System.out.println("odd number sum "+oddsum);
+    }
+}
