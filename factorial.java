3921dc2d6724564719d4c1bd7f6d9a01f372b2bd
Prathameshn2003 <prathameshnalawade288@gmail.com>
2024-08-17 15:02:38 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class factorial{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println("table of "+num+" is");
+
+        int table;
+        for(int i=1;i<=10;i++){
+            table=num*i;
+            System.out.println(+table);
+        }
+        
+    
+}}
