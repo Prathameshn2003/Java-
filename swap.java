f29727b2bd01a11fa39fe01a475dec91137b6706
Prathameshn2003 <prathameshnalawade288@gmail.com>
2024-08-18 14:29:51 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+public class swap{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter array size");
+    int size=sc.nextInt();
+    int array[]=new int[size];
+    int evensum=0;
+    int oddsum=0;
+    for(int i=0;i<size;i++){
+        System.out.println("Enter array element");
+         array[i]=sc.nextInt();
+        if(array[i]%2==0)
+        {
+          evensum=evensum+array[i];
+        } else{
+          oddsum=oddsum+array[i];
+        }    
+
+    }
+    System.out.println("sum of even number "+evensum);
+    System.out.println("sum of odd number"+oddsum);
+
+    
+  }
+}
