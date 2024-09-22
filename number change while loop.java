e8dfccdacc755085793fdf2ded81127ac7e73de3
Prathameshn2003 <prathameshnalawade288@gmail.com>
2024-09-22 15:46:11 +0530

    Update and rename j.java to number change while loop.java

--- number change while loop.java
+++ number change while loop.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class j{
+public class change{
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
@@ -8,8 +8,6 @@ public class j{
         {
             int rem=num%10;
             System.out.print(rem);
-
-
              num=num/10;
         }
     }
