ad3c23d03868f21bc1ea0dd2287ae7ff05848d9b
Prathameshn2003 <prathameshnalawade288@gmail.com>
2024-09-22 16:51:35 +0530

    Add files via upload

---
+++ j.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class j{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt(); 
+        int array[]=new int[size];
+        int count=0;
+        for(int i=0;i<size;i++)
+        {
+            array[i]=sc.nextInt();
+
+            while(array[i]>0)
+            {
+                int rem=array[i]%10;
+                count++;
+                 array[i]=array[i]/10;
+            }
+
+        }
+        System.out.println("total array count"+count);
+
+
+         
+            
+        
+        
+        
+            
+
+    
+}}
